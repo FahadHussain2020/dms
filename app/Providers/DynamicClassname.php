326ee42ecb12e3e2ba1f153ee505028329d2242d
FahadHussain2020 <fahadhussain2020@gmail.com>
2021-01-30 15:23:26 +0500

    commit

--- app/Providers/DynamicClassname.php
+++ app/Providers/DynamicClassname.php
@@ -1,13 +1,13 @@
 <?php
-namespace App\Providers;
-// use App\Classmodel; // write model name here
+namespace App;
+use App\Classmodel; // write model name here
 use Illuminate\Support\ServiceProvider;
 class DynamicClassname extends ServiceProvider
 {
     public function boot()
     {
         view()->composer('*',function($view){
-            $view->with('classname_array', \Classmodel::all());
+            $view->with('classname_array', Classmodel::all());
         });
     }
 

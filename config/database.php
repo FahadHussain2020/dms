36960359e1fd4a101db20da4a5110108d92d01c8
FahadHussain2020 <fahadhussain2020@gmail.com>
2021-01-30 15:47:27 +0500

    commit

--- config/database.php
+++ config/database.php
@@ -42,12 +42,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => '127.0.0.1',
             'port'      => '33060',//your mysql port,
-            'database' => env('DB_DATABASE', 'dms'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'database' => 'dms',
+            'username' => 'root',
+            'password' => '',
+            'unix_socket' => '',
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',

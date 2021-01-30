b3893563d880e7b3592abb4a2a06af76e246fb80
FahadHussain2020 <fahadhussain2020@gmail.com>
2021-01-30 15:48:53 +0500

    commit

--- config/database.php
+++ config/database.php
@@ -42,12 +42,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => '127.0.0.1',
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port'      => '33060',//your mysql port,
-            'database' => 'dms',
-            'username' => 'root',
-            'password' => '',
-            'unix_socket' => '',
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
